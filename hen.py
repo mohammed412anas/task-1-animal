97b591c0e6f29adf83276c157409fe48bfdb4c56
mohammed412anas <mohammed.anas@everest.engineering>
2025-06-05 11:35:52 +0530

    Crate Hen

---
+++ hen.py
@@ -0,0 +1,12 @@
+class Cow:
+    def eat(self):
+        print("I can Eat food")
+
+    def sleep(self):
+        print("I can Sleep in fields")
+
+    def sound(self):
+        print("I can Sound ambaa")
+
+    def run(self):
+        print("I can Run on road")
